6b90d3f8fc564c8e5110afc8f2a1a7bb766768a6
Sam Ellicott <sellicott@cedarville.edu>
2021-09-11 09:44:05 -0400

    removed 64-bit instructions use human readable register names

--- riscv32-tok.h
+++ riscv32-tok.h
@@ -3,81 +3,47 @@
 
 // See https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf
 
-/* register */
-
- DEF_ASM(x0)
- DEF_ASM(x1)
- DEF_ASM(x2)
- DEF_ASM(x3)
- DEF_ASM(x4)
- DEF_ASM(x5)
- DEF_ASM(x6)
- DEF_ASM(x7)
- DEF_ASM(x8)
- DEF_ASM(x9)
- DEF_ASM(x10)
- DEF_ASM(x11)
- DEF_ASM(x12)
- DEF_ASM(x13)
- DEF_ASM(x14)
- DEF_ASM(x15)
- DEF_ASM(x16)
- DEF_ASM(x17)
- DEF_ASM(x18)
- DEF_ASM(x19)
- DEF_ASM(x20)
- DEF_ASM(x21)
- DEF_ASM(x22)
- DEF_ASM(x23)
- DEF_ASM(x24)
- DEF_ASM(x25)
- DEF_ASM(x26)
- DEF_ASM(x27)
- DEF_ASM(x28)
- DEF_ASM(x29)
- DEF_ASM(x30)
- DEF_ASM(x31)
+/* registers */
 
 /* register macros */
 
- DEF_ASM(zero)
-/*
- DEF_ASM(ra)
- DEF_ASM(sp)
- DEF_ASM(gp)
- DEF_ASM(tp)
- DEF_ASM(t0)
- DEF_ASM(t1)
- DEF_ASM(t2)
- DEF_ASM(fp)
- DEF_ASM(s1)
- DEF_ASM(a0)
- DEF_ASM(a1)
- DEF_ASM(a2)
- DEF_ASM(a3)
- DEF_ASM(a4)
- DEF_ASM(a5)
- DEF_ASM(a6)
- DEF_ASM(a7)
- DEF_ASM(s2)
- DEF_ASM(s3)
- DEF_ASM(s4)
- DEF_ASM(s5)
- DEF_ASM(s6)
- DEF_ASM(s7)
- DEF_ASM(s8)
- DEF_ASM(s9)
- DEF_ASM(s10)
- DEF_ASM(s11)
- DEF_ASM(t3)
- DEF_ASM(t4)
- DEF_ASM(t5)
- DEF_ASM(t6)
-
- DEF_ASM(s0) // = x8
-*/
+ DEF_ASM(zero)  // x0
+ DEF_ASM(ra)    // x1
+ DEF_ASM(sp)    // x2
+ DEF_ASM(gp)    // x3
+ DEF_ASM(tp)    // x4
+ DEF_ASM(t0)    // x5
+ DEF_ASM(t1)    // x6
+ DEF_ASM(t2)    // x7
+ DEF_ASM(fp)    // x8
+ DEF_ASM(s1)    // x9
+ DEF_ASM(a0)    // x10
+ DEF_ASM(a1)    // x11
+ DEF_ASM(a2)    // x12
+ DEF_ASM(a3)    // x13
+ DEF_ASM(a4)    // x14
+ DEF_ASM(a5)    // x15
+ DEF_ASM(a6)    // x16
+ DEF_ASM(a7)    // x17
+ DEF_ASM(s2)    // x18
+ DEF_ASM(s3)    // x19
+ DEF_ASM(s4)    // x20
+ DEF_ASM(s5)    // x21
+ DEF_ASM(s6)    // x22
+ DEF_ASM(s7)    // x23
+ DEF_ASM(s8)    // x24
+ DEF_ASM(s9)    // x25
+ DEF_ASM(s10)   // x26
+ DEF_ASM(s11)   // x27
+ DEF_ASM(t3)    // x28
+ DEF_ASM(t4)    // x29
+ DEF_ASM(t5)    // x30
+ DEF_ASM(t6)    // x31
 
  DEF_ASM(pc)
+
+
+ //DEF_ASM(s0) // = x8
 
 #define DEF_ASM_WITH_SUFFIX(x, y) \
   DEF(TOK_ASM_ ## x ## _ ## y, #x #y)
@@ -89,18 +55,12 @@
  DEF_ASM(lw)
  DEF_ASM(lbu)
  DEF_ASM(lhu)
- DEF_ASM(ld)
- DEF_ASM(lq)
- DEF_ASM(lwu)
- DEF_ASM(ldu)
 
 /* Stores */
 
  DEF_ASM(sb)
  DEF_ASM(sh)
  DEF_ASM(sw)
- DEF_ASM(sd)
- DEF_ASM(sq)
 
 /* Shifts */
 
@@ -111,19 +71,6 @@
  DEF_ASM(sra)
  DEF_ASM(srai)
 
- DEF_ASM(sllw)
- DEF_ASM(slld)
- DEF_ASM(slliw)
- DEF_ASM(sllid)
- DEF_ASM(srlw)
- DEF_ASM(srld)
- DEF_ASM(srliw)
- DEF_ASM(srlid)
- DEF_ASM(sraw)
- DEF_ASM(srad)
- DEF_ASM(sraiw)
- DEF_ASM(sraid)
-
 /* Arithmetic */
 
  DEF_ASM(add)
@@ -131,13 +78,6 @@
  DEF_ASM(sub)
  DEF_ASM(lui)
  DEF_ASM(auipc)
-
- DEF_ASM(addw)
- DEF_ASM(addd)
- DEF_ASM(addiw)
- DEF_ASM(addid)
- DEF_ASM(subw)
- DEF_ASM(subd)
 
 /* Logical */
 
