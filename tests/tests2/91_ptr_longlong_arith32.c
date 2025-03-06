1e345db9a5af19a53248da53bf90cf7b498ddc0b
Sam Ellicott <sellicott@cedarville.edu>
2025-03-05 21:44:16 -0500

    add some potentially better tests for 64-bit add/sub emulation

--- tests/tests2/91_ptr_longlong_arith32.c
+++ tests/tests2/91_ptr_longlong_arith32.c
@@ -1,15 +1,29 @@
-int printf(const char *, ...);
+int printf( const char *, ... );
 char t[] = "012345678";
 
-int main(void)
+int main( void )
 {
     char *data = t;
     unsigned long long r = 4;
     unsigned a = 5;
     unsigned long long b = 12;
+    unsigned long long c = 0;
+    unsigned long long d = 1;
 
-    *(unsigned*)(data + r) += a - b;
+    *(unsigned *)( data + r ) += a - b;
+    printf( "data = \"%s\"\n", data );
 
-    printf("data = \"%s\"\n", data);
+    // try and overflow the lower part of a 32-bit variable
+    // test carry
+    c = 0xffffffff;
+    c += d;
+    printf( "c = \"%lld\"\n", c );
+
+    // try and underflow the lower part of a 32-bit variable
+    // test carry
+    c = 0x100000000;
+    c -= d;
+    printf( "c = \"%lld\"\n", c );
+
     return 0;
 }
