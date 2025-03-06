93078ecbd27fad9d7e88016e2ea583bc08981740
Sam Ellicott <sellicott@cedarville.edu>
2025-03-06 03:37:54 -0500

    updated test91

--- tests/tests2/91_ptr_longlong_arith32.c
+++ tests/tests2/91_ptr_longlong_arith32.c
@@ -7,23 +7,45 @@ int main( void )
     unsigned long long r = 4;
     unsigned a = 5;
     unsigned long long b = 12;
+
+    asm( "nop; nop; nop;" );
     unsigned long long c = 0;
     unsigned long long d = 1;
+    unsigned long long e = 0;
+    asm( "nop; nop; nop;" );
+
 
     *(unsigned *)( data + r ) += a - b;
     printf( "data = \"%s\"\n", data );
 
+    // print starting values
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n", c, d, e );
+    printf( "c = \"%llx\", ", c );
+    printf( "d = \"%llx\", ", d );
+    printf( "e = \"%llx\"\n", e );
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n\n", c, d, e );
+
     // try and overflow the lower part of a 32-bit variable
     // test carry
+    asm( "nop; nop; nop;" );
     c = 0xffffffff;
-    c += d;
-    printf( "c = \"%lld\"\n", c );
+    asm( "nop; nop; nop;" );
+    e = c + d;
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n", c, d, e );
+    printf( "c = \"%llx\", ", c );
+    printf( "d = \"%llx\", ", d );
+    printf( "e = \"%llx\"\n", e );
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n\n", c, d, e );
 
     // try and underflow the lower part of a 32-bit variable
     // test carry
     c = 0x100000000;
-    c -= d;
-    printf( "c = \"%lld\"\n", c );
+    e = c - d;
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n", c, d, e );
+    printf( "c = \"%llx\", ", c );
+    printf( "d = \"%llx\", ", d );
+    printf( "e = \"%llx\"\n", e );
+    printf( "c = \"%llx\", d = \"%llx\", e = \"%llx\"\n\n", c, d, e );
 
     return 0;
 }
